846b1c3156c51a8129fbd8890cbd02d6e9ffd94e
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2024-06-18 14:55:24 -0300

    remove unused method

--- app/expense/expense.py
+++ app/expense/expense.py
@@ -8,9 +8,4 @@ class Expense(BaseModel):
     value:float
     expenseDate:date
 
-    @classmethod
-    def fromTuple(cls, tpl):
-        print({k: v for k, v in zip(cls.model_fields.keys(), tpl)})
-        return cls(**{k: v for k, v in zip(cls.model_fields.keys(), tpl)})
-
 insertScriptExpense = text('INSERT INTO fato_expense VALUES (:idFrequencyType,:idExpenseSubCategory,:value,:expenseDate);')
