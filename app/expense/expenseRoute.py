d455100701c6793e3f0d9f2173152af84ef9e797
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2024-06-19 11:16:18 -0300

    return an object directly

--- app/expense/expenseRoute.py
+++ app/expense/expenseRoute.py
@@ -13,8 +13,7 @@ async def addExpense(e:Expense) -> Expense:
     return e
 
 @expenses.get("/")
-async def getExpenseAll():
+async def getExpenseAll() -> list[Expense]:
     async with engine.connect() as conn:
         res = await conn.execute(text("SELECT * FROM fato_expense;"))
-        fields = Expense.model_fields
-        return [Expense(**{k:v for k,v in zip(fields, e)}).model_dump() for e in res.fetchall()]
+        return [Expense(**{k:v for k,v in zip(Expense.model_fields, e)}) for e in res.fetchall()]
