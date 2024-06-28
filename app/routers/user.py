42780f7fb442f6d50cf5633ed71d9ef6cf0605c9
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2024-06-28 11:18:22 -0300

    login

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,14 +1,17 @@
 from passlib.context import CryptContext
+import jwt
 
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi import APIRouter, Depends, HTTPException, status
 from database import getConn, engine, text
+from typing import Annotated
 from models import User
 
-users = APIRouter(prefix="/users",tags=["users"])
+SECRET_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJVc2VyIjoibGFvcyJ9.MvNzpRt3bxwP5EQkLX-aTxXFRm_IDU5DeooOnH0UvIk"
+ALGORITHM = "HS256"
 
+users = APIRouter(prefix="/users",tags=["users"])
 pwdContext = CryptContext(schemes=["bcrypt"])
-async def verifyPassword(plainPassword:str, hashedPassword:str) -> bool:
-    return pwdContext.verify(plainPassword, hashedPassword)
 
 async def getUser(userName:str=None, email:str=None) -> User|None:
     if userName is not None:
@@ -24,17 +27,9 @@ async def getUser(userName:str=None, email:str=None) -> User|None:
     if result:
         return User(**{k:v for k,v in zip(User.model_fields, result[0])})
 
-async def authenticateUser(userName:str, password:str) -> User|bool:
-    u = await getUser(userName=userName)
-    if not u:
-        return False
-    if not await verifyPassword(password, u.password):
-        return False
-    return User
-
-@users.post("", response_model=User)
-@users.post("/", response_model=User)
-async def addUser(u:User, conn=Depends(getConn)) -> User:
+@users.post("/register", response_model=User)
+@users.post("/register/", response_model=User)
+async def register(u:User, conn=Depends(getConn)) -> User:
     insert = u.model_dump()
     if await getUser(userName=insert["userName"]) is not None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="UserName already in use")
@@ -45,3 +40,14 @@ async def addUser(u:User, conn=Depends(getConn)) -> User:
     await conn.execute(u.getInsertScript(), insert)
     await conn.commit()
     return u
+
+@users.post("/login", response_model=User)
+@users.post("/login/", response_model=User)
+async def login(formData:Annotated[OAuth2PasswordRequestForm, Depends()]) -> str:
+    user = getUser(userName=formData.username)
+    if await user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="UserName incorrect.")
+    elif not pwdContext.verify(formData.password, user.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Password incorrect.")
+    else:
+        return jwt.encode({"sub":user.userName}, SECRET_KEY, ALGORITHM)
